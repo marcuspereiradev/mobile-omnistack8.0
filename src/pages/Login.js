5c3f63591ebc2b6ac24c647cd55d562c8991123b
marcuspereiradev <vinnycoast@gmail.com>
2019-08-11 17:18:21 -0300

    Starting the project

---
+++ src/pages/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { View, Text, StyleSheet } from 'react-native';
+
+const Login = () => {
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    
+  },
+ 
+});
+
+export default Login;
