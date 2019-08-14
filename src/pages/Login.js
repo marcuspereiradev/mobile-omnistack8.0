22a674f129469f8ccd9003b503a02fe9121f8c2d
marcuspereiradev <vinnycoast@gmail.com>
2019-08-14 12:27:31 -0300

    Fix reload. Keep the id logged when reload

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 import API from '../services/Api';
 
 import { View, Image, StyleSheet, TextInput, TouchableOpacity, Text } from 'react-native';
@@ -8,10 +9,21 @@ import logo from '../assets/logo.png';
 const Login = ({ navigation }) => {
   const [user, setUser] = useState('');
 
+  useEffect(() => {
+    AsyncStorage.getItem('user').then((user) => {
+      if (user) {
+        navigation.navigate('Main', { user })
+      }
+    })
+  }, []);
+
   async function handleLogin() {
     const response = await API.post('/devs', { username: user });
+
     const { _id } = response.data;
-    console.log(_id);
+
+    await AsyncStorage.setItem('user', _id);
+
     navigation.navigate('Main', { _id });
   }
 
