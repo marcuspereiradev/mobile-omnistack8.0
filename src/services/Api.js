03a06b2f192cd1398a51f462b1be3f9b3f77461d
marcuspereiradev <vinnycoast@gmail.com>
2019-08-14 12:28:24 -0300

    To access api url in the Android Studio emulator, change localhost by 10.0.2.2

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,8 @@
+// To access api url in the Android Studio emulator, change localhost by 10.0.2.2 
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL: 'http://10.0.2.2:3333'
 });
 
 export default API;
